d812433d07df900278bb27a405a93a9e4a422576
Dustin G <Dustin.Guillen14@yahoo.com>
2024-06-24 17:14:14 -0600

    [ticket-number] fix nav sin-in/sign-out bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :people
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    post '/users/sign_up' => 'devise/registrations#create'
+  end
+
   root "home#index"
 
   get "about", to: "about#index"
